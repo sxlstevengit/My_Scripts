a141af01ff616a8ccda466a84f94926f6c308c2f
sxlstevengit <sxl_youcun@qq.com>
2018-09-26 16:27:38 +0800

    add some scripts
    add some scripts and update

--- jdk/install_jdk.sh
+++ jdk/install_jdk.sh
@@ -12,8 +12,8 @@ install_jdk(){
   cat >> $ENV_FILE <<- EOF
 JAVA_HOME=/usr/local/java/$JdkVersion
 JRE_HOME=$JAVA_HOME/jre
-PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
-CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JRE_HOME/lib
+PATH=\$PATH:\$JAVA_HOME/bin:\$JRE_HOME/bin
+CLASSPATH=.:\$JAVA_HOME/lib/dt.jar:\$JAVA_HOME/lib/tools.jar:\$JRE_HOME/lib
 export JAVA_HOME JRE_HOME PATH CLASSPATH
 EOF
   . $ENV_FILE

f46b968923a6831aa6cab2f3a273184b1932b4a8
sxlstevengit <sxl_youcun@qq.com>
2020-10-12 11:18:04 +0800

    Update calc_count.sh
    统计添加格式化输出，更加易读。

--- nginx/log 统计/calc_count.sh
+++ nginx/log 统计/calc_count.sh
@@ -19,4 +19,4 @@ egrep -r cloudwalk-portal XXX_202010*.log | awk '{print $11}' | sort | uniq -c |
 #awk '/game-portal/{print $11}'XXX_202010*.log | wc -l | xargs echo "game-portal": >> result.txt   
 
 #统计该接口下面细分的接口次数,并倒序排列
-awk '/game-portal/{print $11}' XXX_202010*.log | awk '{++a[$NF]}END{for(b in a )print a[b],b}' | sort -nr >> result.txt
+awk '/game-portal/{print $11}' XXX_202010*.log | awk '{++a[$NF]}END{for(b in a )print a[b],b}' | sort -nr | awk 'BEGIN{printf "game-portal接口统计如下:\n\n"}{print $0}'>> result.txt
